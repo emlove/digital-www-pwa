4fb950824a2c3bb9bd282153845aa751c972b2e5
DJ Meph <info@djmeph.net>
2025-01-05 23:17:11 +0000

    Link Component

---
+++ libs/link/component/src/lib/link-component.tsx
@@ -0,0 +1,11 @@
+import styles from './link-component.module.css';
+
+export function Link() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to LinkComponent!</h1>
+    </div>
+  );
+}
+
+export default Link;
