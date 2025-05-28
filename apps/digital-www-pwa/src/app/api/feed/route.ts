dfb57294075fae648c6805e1f1d4bd794a7b5ad7
DJ Meph <norm.witte@gmail.com>
2025-05-28 02:39:08 -0400

    Invalidate feed cache every 1 hour (#37)
    Co-authored-by: DJ Meph <info@djmeph.net>

--- apps/digital-www-pwa/src/app/api/feed/route.ts
+++ apps/digital-www-pwa/src/app/api/feed/route.ts
@@ -1,15 +1,13 @@
 async function fetchData(dataType: string) {
-  const response = await fetch(`https://whatwherewhen.lakesoffire.org/${dataType}.json`);
+  const response = await fetch(
+    `https://whatwherewhen.lakesoffire.org/${dataType}.json`,
+    { next: { revalidate: 3600 } }
+  );
   const data = await response.json();
   return data.coalesce.coalesce;
 }
 export async function GET(request: Request) {
-  const [
-    events,
-    camps,
-    locations,
-    departments,
-  ] = await Promise.all([
+  const [events, camps, locations, departments] = await Promise.all([
     fetchData('events'),
     fetchData('camps'),
     fetchData('locations'),
